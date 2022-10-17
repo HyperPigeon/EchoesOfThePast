cddf7aa1bc9e688a8cfc7957294ccf79cb40e2df
HyperPigeon <58276133+HyperPigeon@users.noreply.github.com>
2022-10-17 15:28:49 -0400

    removed unused interaction bypass tag key

--- src/main/java/net/hyper_pigeon/echoes_of_the_past/register/EchoesOfThePastTagKeys.java
+++ src/main/java/net/hyper_pigeon/echoes_of_the_past/register/EchoesOfThePastTagKeys.java
@@ -10,8 +10,5 @@ public class EchoesOfThePastTagKeys {
     private static final Identifier BLACKLIST_ID = new Identifier("echoes_of_the_past:blacklist");
     public static final TagKey<EntityType<?>> BLACKLIST = TagKey.of(Registry.ENTITY_TYPE_KEY,BLACKLIST_ID);
 
-    private static final Identifier INTERACTION_BYPASS_ID =
-            new Identifier("echoes_of_the_past:interaction_bypass");
-    public static final TagKey<EntityType<?>> INTERACTION_BYPASS = TagKey.of(Registry.ENTITY_TYPE_KEY, INTERACTION_BYPASS_ID);
 
 }
